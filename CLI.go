38be8c48c0815dfa834b7dd1cdb9fd547922d69d
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2025-03-01 20:35:41 +0100

    feat: schedule multiple blinds

--- CLI.go
+++ CLI.go
@@ -21,6 +21,7 @@ func NewCLI(store PlayerStore, in io.Reader, alerter BlindAlerter) *CLI {
 	return &CLI{playerStore: store, in: bufio.NewScanner(in), alerter: alerter}
 }
 func (cli *CLI) PlayPoker() {
+	cli.scheduleBlindAlerts()
 	cli.alerter.ScheduleAlertAt(5*time.Second, 100)
 	userInput := cli.readLine()
 	cli.playerStore.RecordWin(extractWinner(userInput))
@@ -29,6 +30,15 @@ func (cli *CLI) PlayPoker() {
 func (cli *CLI) readLine() string {
 	cli.in.Scan()
 	return cli.in.Text()
+}
+
+func (cli *CLI) scheduleBlindAlerts() {
+	blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
+	blindTime := 0 * time.Second
+	for _, blind := range blinds {
+		cli.alerter.ScheduleAlertAt(blindTime, blind)
+		blindTime = blindTime + 10*time.Minute
+	}
 }
 
 func extractWinner(userInput string) string {
